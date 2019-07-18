3172b671528c219678e6856ccca8cd11f9b5c19d
noam <noam@kuandeng.com>
2019-07-18 13:49:59 +0800

    adam optimizer params

--- architect.py
+++ architect.py
@@ -9,7 +9,7 @@ class Architect () :
         self.network_weight_decay = args.weight_decay
         self.model = model
         self.optimizer = torch.optim.Adam(self.model.arch_parameters(),
-            lr=args.arch_lr, betas=(0.5, 0.999), weight_decay=args.arch_weight_decay)
+            lr=args.arch_lr, betas=(0.9, 0.999), weight_decay=args.arch_weight_decay)
 
     def step (self, input_valid, target_valid) :
         self.optimizer.zero_grad ()
