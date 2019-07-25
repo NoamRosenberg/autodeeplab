d1ede6fe6389045cf58a3336dde009b5ce327737
noam <noam@kuandeng.com>
2019-07-25 10:49:59 +0800

    default filter multiplier to 8

--- train_cityscapes.sh
+++ train_cityscapes.sh
@@ -1,2 +1,2 @@
 CUDA_VISIBLE_DEVICES=0,1 python train_autodeeplab.py --batch-size 2 \
---dataset cityscapes --checkname July22  --alpha_epoch 20 --filter_multiplier 4
+--dataset cityscapes --checkname July22  --alpha_epoch 20 --filter_multiplier 8
