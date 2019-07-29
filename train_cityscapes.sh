c0827513461bb6aa45871a9b3d6df790222149b4
noam <noam@kuandeng.com>
2019-07-29 11:09:18 +0800

    new resize params for test

--- train_cityscapes.sh
+++ train_cityscapes.sh
@@ -1,2 +1,2 @@
 CUDA_VISIBLE_DEVICES=0,1 python train_autodeeplab.py --batch-size 2 \
---dataset cityscapes --checkname July22  --alpha_epoch 20 --filter_multiplier 8
+--dataset cityscapes --checkname July29_newbetas_branch  --alpha_epoch 20 --filter_multiplier 4 --resize 358 --crop_size 224
