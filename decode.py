31d12ae1f8db5623501242554c183fb6794e47bb
noam <noam@kuandeng.com>
2019-08-01 17:20:06 +0800

    Viterbi in progress 2

--- decode.py
+++ decode.py
@@ -2,38 +2,14 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 
-
 class ViterbiDecoder(object):
-    def __init__(self, initialProb, transProb, obsProb):
-        self.N = initialProb.shape[0]
-        self.initialProb = initialProb
-        self.transProb = transProb
-        self.obsProb = obsProb
-        assert self.initialProb.shape == (self.N, 1)
-        assert self.transProb.shape == (self.N, self.N)
-        assert self.obsProb.shape[0] == self.N
+    def __init__(self):
+        self._num_layers = 12
+        self.bottom_betas = torch.tensor(1e-3 * torch.randn(self._num_layers - 1, 2).cuda(), requires_grad=True)
+        self.betas8 = torch.tensor(1e-3 * torch.randn(self._num_layers - 2, 3).cuda(), requires_grad=True)
+        self.betas16 = torch.tensor(1e-3 * torch.randn(self._num_layers - 3, 3).cuda(), requires_grad=True)
+        self.top_betas = torch.tensor(1e-3 * torch.randn(self._num_layers - 1, 2).cuda(), requires_grad=True)
 
-    def Obs(self, obs):
-        return self.obsProb[:, obs, None]
-
-    def Decode(self, obs):
-        trellis = np.zeros((self.N, len(obs)))
-        backpt = np.ones((self.N, len(obs)), 'int32') * -1
-
-        # initialization
-        trellis[:, 0] = np.squeeze(self.initialProb * self.Obs(obs[0]))
-
-        for t in range(1, len(obs)):
-            trellis[:, t] = (trellis[:, t - 1, None].dot(self.Obs(obs[t]).T) * self.transProb).max(0)
-            backpt[:, t] = (np.tile(trellis[:, t - 1, None], [1, self.N]) * self.transProb).argmax(0)
-        # termination
-        tokens = [trellis[:, -1].argmax()]
-        for i in range(len(obs) - 1, 0, -1):
-            tokens.append(backpt[tokens[-1], i])
-        return tokens[::-1]
-
-class ViterbiDecoder(object):
-    def __init__(self, bottom_betas, betas8, betas16, top_betas):
         normalized_bottom_betas = F.softmax(self.bottom_betas, dim=-1)
         normalized_betas8 = F.softmax (self.betas8, dim = -1)
         normalized_betas16 = F.softmax(self.betas16, dim=-1)
@@ -53,16 +29,16 @@ class ViterbiDecoder(object):
         self.network_space[2, 3, 0] = 1.
 
         self.network_space[3:, 2, :] = normalized_betas16
-        self.network_space[3:, 3, :] = normalized_top_betas[2:]
+        self.network_space[3:, 3, :2] = normalized_top_betas[2:]
 
     def decode(self):
 
-        #best_paths_space = torch.argmax(self.network_space, dim=-1)
+        #remember best paths
         paths_space = torch.zeros(self.network_space.shape)
         paths_space[0, 0, 1] = 1.
         paths_space[0, 1, 0] = 1.
+        #update total transition probability
         prob_space = torch.zeros(self.network_space.shape[:2])
-        paths = []
         prob_space[0, 0] = 1.
         prob_space[0, 1] = 1.
         for layer in range(1, paths_space.shape[0]):
@@ -71,13 +47,34 @@ class ViterbiDecoder(object):
                 for i in range(0,3):
                     prev_sample = sample + (i - 1)
                     if prev_sample < 0 or prev_sample > 3:
+                        new_probs_ls.append(torch.tensor(0.))
                         continue
-                    #clamp
-                    #prev_sample = max(min(sample + (i-1), 0),3)
-                    prev_prob = paths_space[layer - 1, prev_sample]
+                    prev_prob = prob_space[layer - 1, prev_sample]
                     curr_prob = self.network_space[layer, sample, i]
                     new_prob = prev_prob * curr_prob
                     new_probs_ls.append(new_prob)
-                    new_probs_tls = torch.tensor(new_probs_ls)
-                new_probs_tls
-                paths_space[layer, sample] = new_probs_tls.max()
+                new_probs_tls = torch.tensor(new_probs_ls)
+                if new_probs_tls[new_probs_tls > 0].shape[0] == 0:
+                    continue
+                prob_space[layer, sample] = new_probs_tls.max()
+                best_index = new_probs_tls.argmax()
+                paths_space[layer, sample, best_index] = 1.
+
+        #pick the branch with the highest probability
+        all_samples = torch.tensor([i for i in range(prob_space.shape[-1])])
+        best_sample = torch.argmax(prob_space[-1])
+        other_samples = all_samples[all_samples != best_sample]
+        paths_space[-1, other_samples] = 0.
+        i = paths_space[-1, best_sample].nonzero()[0, 0]
+        prev_sample = best_sample + (i - 1)
+
+        #follow the branch and eliminate all other branches
+        for layer in range(prob_space.shape[0] - 2, -1, -1):
+            other_samples = all_samples[all_samples != prev_sample]
+            paths_space[layer, other_samples] = 0.
+            i = paths_space[layer, prev_sample].nonzero()[0, 0]
+            prev_sample = prev_sample + (i - 1)
+
+if __name__ == '__main__':
+    viterbi = ViterbiDecoder()
+    decode = viterbi.decode()
