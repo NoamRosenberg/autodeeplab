1f0088f12ad837d2d0a94dcdd18a9597af94ce8c
zxzx2999 <853282226@qq.com>
2019-09-26 23:26:14 +0800

    fix abn and dataset

--- train_distributed.py
+++ train_distributed.py
@@ -1,10 +1,183 @@
 import os
-import sys
-
-import torch
-
+import numpy as np
+from tqdm import tqdm
+from mypath import Path
+import torch.distributed as dist
+from dataloaders import make_data_loader
+from modeling.sync_batchnorm.replicate import patch_replication_callback
+from modeling.deeplab import *
+from utils.loss import build_criterion
+from utils.calculate_weights import calculate_weigths_labels
+from utils.lr_scheduler import LR_Scheduler
+from utils.saver import Saver
+from utils.summaries import TensorboardSummary
+from utils.metrics import Evaluator
 from config_utils.re_train_autodeeplab import obtain_retrain_autodeeplab_args
 
+
+class Trainer(object):
+    def __init__(self, args):
+        self.args = args
+
+        # Define Saver
+        self.saver = Saver(args)
+        self.saver.save_experiment_config()
+        # Define Tensorboard Summary
+        self.summary = TensorboardSummary(self.saver.experiment_dir)
+        self.writer = self.summary.create_summary()
+
+        # Define Dataloader
+        kwargs = {'num_workers': args.workers, 'pin_memory': True}
+        self.train_loader, self.val_loader, self.test_loader, self.nclass = make_data_loader(args, **kwargs)
+
+        # Define network
+        model = DeepLab(num_classes=self.nclass,
+                        backbone=args.backbone,
+                        output_stride=args.out_stride,
+                        sync_bn=args.sync_bn,
+                        freeze_bn=args.freeze_bn)
+
+        model = nn.parallel.DistributedDataParallel(model, device_ids=[args.local_rank, ],
+                                                    output_device=args.local_rank)
+
+        # Define Optimizer
+        optimizer = torch.optim.SGD(model.parameters(), momentum=args.momentum,
+                                    weight_decay=args.weight_decay, nesterov=args.nesterov)
+
+        # Define Criterion
+        # whether to use class balanced weights
+        if args.use_balanced_weights:
+            classes_weights_path = os.path.join(Path.db_root_dir(args.dataset), args.dataset + '_classes_weights.npy')
+            if os.path.isfile(classes_weights_path):
+                weight = np.load(classes_weights_path)
+            else:
+                weight = calculate_weigths_labels(args.dataset, self.train_loader, self.nclass)
+            weight = torch.from_numpy(weight.astype(np.float32))
+        else:
+            weight = None
+        self.args.weight = weight
+        if self.args.criterion == 'Ohem':
+            args.thresh = 0.7
+            args.n_min = (self.args.batch_size / len(args.gpu_ids)) * args.crop_size[0] * args.crop_size[1] // 16
+        self.criterion = build_criterion(args)
+        self.model, self.optimizer = model, optimizer
+
+        # Define Evaluator
+        self.evaluator = Evaluator(self.nclass)
+        # Define lr scheduler
+        self.scheduler = LR_Scheduler(args.lr_scheduler, args.lr,
+                                      args.epochs, len(self.train_loader))
+
+        # Using cuda
+        if args.cuda:
+            self.model = nn.DataParallel(self.model, device_ids=self.args.gpu_ids)
+            patch_replication_callback(self.model)
+            self.model = self.model.cuda()
+
+        # Resuming checkpoint
+        self.best_pred = 0.0
+        if args.resume is not None:
+            if not os.path.isfile(args.resume):
+                raise RuntimeError("=> no checkpoint found at '{}'".format(args.resume))
+            checkpoint = torch.load(args.resume)
+            args.start_epoch = checkpoint['epoch']
+            if args.cuda:
+                self.model.module.load_state_dict(checkpoint['state_dict'])
+            else:
+                self.model.load_state_dict(checkpoint['state_dict'])
+            if not args.ft:
+                self.optimizer.load_state_dict(checkpoint['optimizer'])
+            self.best_pred = checkpoint['best_pred']
+            print("=> loaded checkpoint '{}' (epoch {})"
+                  .format(args.resume, checkpoint['epoch']))
+
+        # Clear start epoch if fine-tuning
+        if args.ft:
+            args.start_epoch = 0
+
+    def training(self, epoch):
+        train_loss = 0.0
+        self.model.train()
+        tbar = tqdm(self.train_loader)
+        num_img_tr = len(self.train_loader)
+        for i, sample in enumerate(tbar):
+            image, target = sample['image'], sample['label']
+            if self.args.cuda:
+                image, target = image.cuda(), target.cuda()
+            self.scheduler(self.optimizer, i, epoch, self.best_pred)
+            self.optimizer.zero_grad()
+            output = self.model(image)
+            loss = self.criterion(output, target)
+            loss.backward()
+            self.optimizer.step()
+            train_loss += loss.item()
+            tbar.set_description('Train loss: %.3f' % (train_loss / (i + 1)))
+            self.writer.add_scalar('train/total_loss_iter', loss.item(), i + num_img_tr * epoch)
+
+            # Show 10 * 3 inference results each epoch
+            if i % (num_img_tr // 10) == 0:
+                global_step = i + num_img_tr * epoch
+                self.summary.visualize_image(self.writer, self.args.dataset, image, target, output, global_step)
+
+        self.writer.add_scalar('train/total_loss_epoch', train_loss, epoch)
+        print('[Epoch: %d, numImages: %5d]' % (epoch, i * self.args.batch_size + image.data.shape[0]))
+        print('Loss: %.3f' % train_loss)
+
+        if self.args.no_val:
+            # save checkpoint every epoch
+            is_best = False
+            self.saver.save_checkpoint({
+                'epoch': epoch + 1,
+                'state_dict': self.model.module.state_dict(),
+                'optimizer': self.optimizer.state_dict(),
+                'best_pred': self.best_pred,
+            }, is_best)
+
+    def validation(self, epoch):
+        self.model.eval()
+        self.evaluator.reset()
+        tbar = tqdm(self.val_loader, desc='\r')
+        test_loss = 0.0
+        for i, sample in enumerate(tbar):
+            image, target = sample['image'], sample['label']
+            if self.args.cuda:
+                image, target = image.cuda(), target.cuda()
+            with torch.no_grad():
+                output = self.model(image)
+            loss = self.criterion(output, target)
+            test_loss += loss.item()
+            tbar.set_description('Test loss: %.3f' % (test_loss / (i + 1)))
+            pred = output.data.cpu().numpy()
+            target = target.cpu().numpy()
+            pred = np.argmax(pred, axis=1)
+            # Add batch sample into evaluator
+            self.evaluator.add_batch(target, pred)
+
+        # Fast test during the training
+        Acc = self.evaluator.Pixel_Accuracy()
+        Acc_class = self.evaluator.Pixel_Accuracy_Class()
+        mIoU = self.evaluator.Mean_Intersection_over_Union()
+        FWIoU = self.evaluator.Frequency_Weighted_Intersection_over_Union()
+        self.writer.add_scalar('val/total_loss_epoch', test_loss, epoch)
+        self.writer.add_scalar('val/mIoU', mIoU, epoch)
+        self.writer.add_scalar('val/Acc', Acc, epoch)
+        self.writer.add_scalar('val/Acc_class', Acc_class, epoch)
+        self.writer.add_scalar('val/fwIoU', FWIoU, epoch)
+        print('Validation:')
+        print('[Epoch: %d, numImages: %5d]' % (epoch, i * self.args.batch_size + image.data.shape[0]))
+        print("Acc:{}, Acc_class:{}, mIoU:{}, fwIoU: {}".format(Acc, Acc_class, mIoU, FWIoU))
+        print('Loss: %.3f' % test_loss)
+
+        new_pred = mIoU
+        if new_pred > self.best_pred:
+            is_best = True
+            self.best_pred = new_pred
+            self.saver.save_checkpoint({
+                'epoch': epoch + 1,
+                'state_dict': self.model.module.state_dict(),
+                'optimizer': self.optimizer.state_dict(),
+                'best_pred': self.best_pred,
+            }, is_best)
 
 
 def main():
@@ -40,7 +213,7 @@ def main():
         args.lr = lrs[args.dataset.lower()] / (4 * len(args.gpu_ids)) * args.batch_size
 
     if args.checkname is None:
-        args.checkname = 'deeplab-'+str(args.backbone)
+        args.checkname = 'deeplab-' + str(args.backbone)
     print(args)
     torch.manual_seed(args.seed)
     trainer = Trainer(args)
@@ -52,6 +225,7 @@ def main():
             trainer.validation(epoch)
 
     trainer.writer.close()
+
 
 if __name__ == '__main__':
     main()
--- utils/loss.py
+++ utils/loss.py
@@ -1,5 +1,7 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+
 
 class SegmentationLosses(object):
     def __init__(self, weight=None, size_average=True, batch_average=True, ignore_index=255, cuda=False):
@@ -50,6 +52,44 @@ class SegmentationLosses(object):
 
         return loss
 
+
+class OhemCELoss(nn.Module):
+    def __init__(self, thresh, n_min, ignore_index=255, cuda=False, *args, **kwargs):
+        super(OhemCELoss, self).__init__()
+        self.thresh = thresh
+        self.n_min = n_min
+        self.ignore_lb = ignore_index
+        self.criteria = nn.CrossEntropyLoss(ignore_index=ignore_index)
+        if cuda:
+            self.criteria = self.criteria.cuda()
+
+    def forward(self, logits, labels):
+        N, C, H, W = logits.size()
+        n_pixs = N * H * W
+        logits = logits.permute(0, 2, 3, 1).contiguous().view(-1, C)
+        labels = labels.view(-1)
+        with torch.no_grad():
+            scores = F.softmax(logits, dim=1)
+            labels_cpu = labels
+            invalid_mask = labels_cpu == self.ignore_lb
+            labels_cpu[invalid_mask] = 0
+            picks = scores[torch.arange(n_pixs), labels_cpu]
+            picks[invalid_mask] = 1
+            sorteds, _ = torch.sort(picks)
+            thresh = self.thresh if sorteds[self.n_min] < self.thresh else sorteds[self.n_min]
+            labels[picks > thresh] = self.ignore_lb
+        labels = labels.clone()
+        loss = self.criteria(logits, labels)
+        return loss
+
+
+def build_criterion(args):
+    if args.criterion == 'Ohem':
+        return OhemCELoss(thresh=args.thresh, n_min=args.n_min, cuda=args.cuda)
+    elif args.criterion == 'crossentropy':
+        return SegmentationLosses(weight=args.weight, cuda=args.cuda).build_loss(args.mode)
+
+
 if __name__ == "__main__":
     loss = SegmentationLosses(cuda=True)
     a = torch.rand(1, 3, 7, 7).cuda()
@@ -57,7 +97,3 @@ if __name__ == "__main__":
     print(loss.CrossEntropyLoss(a, b).item())
     print(loss.FocalLoss(a, b, gamma=0, alpha=None).item())
     print(loss.FocalLoss(a, b, gamma=2, alpha=0.5).item())
-
-
-
-
